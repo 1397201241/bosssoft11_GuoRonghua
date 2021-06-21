9a1ba936561f51758bc0405d6da4f83ab141fe5b
okey_upppp <1397201241@qq.com>
2021-06-21 09:54:24 +0800

    购物车状态管理

--- fall_soberly/src/store/modules/cart.js
+++ fall_soberly/src/store/modules/cart.js
@@ -1,3 +1,4 @@
+import {get} from "../../utils/request";
 // initial state
 const state = () => ({
     //商品信息
@@ -8,36 +9,36 @@ const state = () => ({
 
 // getters 相当于计算属性
 const getters = {
-    
-};
-
-// actions 内部可以执行异步操作，context.commit()提交mutations来修改状态
-const actions = {
 
 };
 
 // 提交 mutation 是更改状态的唯一方法，并且这个过程是同步的。
 const mutations = {
+    //设置商品信息
+    setItems(state,items) {
+        state.items=items
+    },
+    //设置购物车信息
+    setCartItems(state,cart_item){
+        state.cart_item=cart_item;
+    },
+};
+
+// actions 内部可以执行异步操作，context.commit()提交mutations来修改状态
+const actions = {
     //获取商品信息
-    getItems(state) {
-        fetch('http://localhost:3000/items')
-            .then(res=>res.json())
-            .then(
-                myJson=>{
-                    state.items=myJson;
-                }
-            ).catch(err=> console.log(err));
+    getItems({commit}) {
+        get('http://localhost:3000/items').then(res=>{
+            commit('setItems',res)
+        }).catch(err=>console.log(err));
     },
     //获取购物车信息
-    getCartItems(state){
-        fetch('http://localhost:3000/cart_item')
-            .then(res=>res.json())
-            .then(
-                myJson=>{
-                    state.cart_item=myJson;
-                }
-            ).catch(err=> console.log(err));
+    getCartItems({commit}){
+        get('http://localhost:3000/cart_item').then(res=>{
+            commit('setCartItems',res)
+        }).catch(err=>console.log(err));
     },
+
 };
 
 export default {
