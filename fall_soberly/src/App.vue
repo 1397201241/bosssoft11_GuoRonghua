223630c5276051c5f886f51d5808188d6657d8f9
okey_upppp <1397201241@qq.com>
2021-06-25 12:14:18 +0800

    使用sessionStorage防止页面刷新时vuex状态丢失

--- fall_soberly/src/App.vue
+++ fall_soberly/src/App.vue
@@ -11,7 +11,18 @@
 
 export default {
   name: 'App',
+  created() {
+    //防止页面刷新时vuex丢失
+    //在页面加载时读取sessionStorage里的状态信息 (保证页面刷新时store的state数据不丢失)
+    if (sessionStorage.getItem("store")) {
+      this.$store.replaceState(Object.assign({},this.$store.state,JSON.parse(sessionStorage.getItem("store"))));
+    }
+    //在页面刷新时（或者关闭）将vuex里的信息保存到sessionStorage里
+    window.addEventListener("beforeunload",()=>{
+      sessionStorage.setItem("store",JSON.stringify(this.$store.state));
+    })
 
+  }
 }
 </script>
 
