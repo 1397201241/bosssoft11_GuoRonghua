1ce037a6c7b1b1866c56abc4f0cdfa890ae75a57
okey_upppp <1397201241@qq.com>
2021-06-25 12:15:31 +0800

    Update main.js

--- fall_soberly/src/main.js
+++ fall_soberly/src/main.js
@@ -11,6 +11,7 @@ import Home from "./views/home/index"
 import Navigation from "./components/Navigation/index";
 import UserList from './views/userList/index';
 import ShoppingCart from './views/shoppingCart/index';
+import ChangeRole from './views/changeRole/index';
 import ItemCard from './views/itemCard/index';
 import OSS from './components/Oss/index';
 import {getToken} from "./utils/auth";
@@ -32,17 +33,22 @@ const routes=[
     },
     {
         path:'/Home',
-        meta:{title:'首页'},
+        meta:{title:'首页',requireAuth:true},
         component:Home,
         children:[
             {
                 path:'UserList',
-                meta:{title:'客户管理'},
+                meta:{title:'客户管理',requireAuth:true},
                 component:UserList
             },
             {
+                path:'ChangeRole',
+                meta:{title:'设置角色',requireAuth:true},
+                component:ChangeRole
+            },
+            {
                 path:'ShoppingCart',
-                meta:{title:'购物车管理'},
+                meta:{title:'购物车管理',requireAuth:true},
                 component:ShoppingCart
             },
             {
@@ -71,10 +77,11 @@ router.beforeEach((to, from, next) => {
         let token=getToken();
         if (token) {
             //判断用户信息是否已获取，这里只能通过长度判断
-            if (store.state['login'].user.length===0){
-                store.dispatch('login/getInfo')
+            if (store.state['login'].info.length===0){
+                store.dispatch('login/getInfo').catch(err=>{
+                    console.log(err)
+                })
             }else{
-                console.log(store.state['login'].user.length);
                 console.log('有了？')
             }
             next()
